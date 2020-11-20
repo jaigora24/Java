556ddda301a2af27a1a970e84d5e9a23a393e5f0
Jai Gora <Jaigora24@gmail.com>
2020-11-20 22:12:06 +0530

    Create GreaterBetweenTwo.java

---
+++ GreaterBetweenTwo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[]args){
+		Scanner sc = new Scanner (System.in);
+	
+	    int firstNumber = sc.nextInt();
+	    int secondNumber = sc.nextInt();
+	    if (firstNumber>secondNumber){
+	        System.out.println("First number is greater than second number");
+	    }
+	    else{
+	        System.out.println("Second number is greater than second number");
+	    }
+		
+	}
+	
+}

351450a4a110fe4f88ac0dfb21f2b0d5c8108357
Jai Gora <Jaigora24@gmail.com>
2020-11-20 22:06:16 +0530

    Create AddTwoNumbersByUserInput.java

---
+++ AddTwoNumbersByUserInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int firstNumber = sc.nextInt(); 
+        int secondNumber = sc.nextInt();
+        int result = firstNumber + secondNumber;
+        System.out.print(result);
+    }
+}

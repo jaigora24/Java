af341c82e8bcc195c07ebb4d13ba0a3999ecc240
Jai Gora <Jaigora24@gmail.com>
2020-11-20 22:08:39 +0530

    Create AreaOfCircle.java

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AreaOfCircle{
+	public static void main(String[]args){
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Enter the radius for the circle: ");
+		float radius = sc.nextFloat();
+		System.out.println(3.141592653589793238 * radius * radius);
+	}
+}

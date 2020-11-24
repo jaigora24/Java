b0bed60b7173fe7dd316f4ecf8b6364bcb464f12
Jai Gora <Jaigora24@gmail.com>
2020-11-24 16:55:16 +0530

    Create FahrenheitToCelcius.java

---
+++ FahrenheitToCelcius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class FarToCelsius{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the temperature (in fahrenheit): ");
+		float fahrenheit = sc.nextFloat();
+        double celsius = ( (fahrenheit)-32)* 0.5555555555555556;
+        System.out.println(celsius);
+	}
+}	

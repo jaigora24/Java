ea7d19d545942d435544cf0f6fa088afd29c379c
Jai Gora <Jaigora24@gmail.com>
2020-11-24 17:00:48 +0530

    Update FahrenheitToCelcius.java

--- FahrenheitToCelcius.java
+++ FahrenheitToCelcius.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class FarToCelsius{
+public class FahrenheitToCelsius{
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the temperature (in fahrenheit): ");

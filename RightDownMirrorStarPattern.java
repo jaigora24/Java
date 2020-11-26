90265632cbb511aad6e57116fee1e743706b0d4b
Jai Gora <Jaigora24@gmail.com>
2020-11-26 19:25:41 +0530

    Create RightDownMirrorStarPattern.java

---
+++ RightDownMirrorStarPattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i-1; j++){
+                System.out.print("  ");
+            }
+            for (int j=1; j<=n-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

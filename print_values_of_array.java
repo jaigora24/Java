94c438cad062bc9f93ae8ee1badb8bf4d2d30402
Jai Gora <Jaigora24@gmail.com>
2021-01-29 00:47:50 +0530

    Create print_values_of_array.java

---
+++ print_values_of_array.java
@@ -0,0 +1,10 @@
+public class print_values_of_arrays{
+	public static void main(String[] args){
+		
+		int age[]={18,29,78,56,45};
+		
+		for(int i=0; i<age.length; i++){
+			System.out.println(age[i]);
+		}
+	}
+}

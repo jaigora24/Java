cbb475c11b37b0e1a110946ba971b32fa9ec21d2
Jai Gora <Jaigora24@gmail.com>
2021-01-29 00:30:58 +0530

    Create intro_to_arrays.java

---
+++ intro_to_arrays.java
@@ -0,0 +1,8 @@
+public class basic_array{
+	public static void main(String[] args){
+		
+		int array[] = {1,2,4,5,3,5,7,6,};
+		System.out.println(array[2]);
+	}
+}
+//Output is "4" because the indexes of elements in Java array is always starts with 0

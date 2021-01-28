d613080cbbd52d9aafb0023e4344d2ebc0954275
Jai Gora <Jaigora24@gmail.com>
2021-01-29 00:36:11 +0530

    Create change_value_in_array.java

---
+++ change_value_in_array.java
@@ -0,0 +1,9 @@
+public class change_value_in_array{
+	public static void main(String[] args){
+		
+		int array[] = {1,2,4,5,3,5,7,6,};
+		array[2]=12;    // from this method we can update the value of array
+		System.out.println(array[2]);
+	}
+}
+//Output is "12"

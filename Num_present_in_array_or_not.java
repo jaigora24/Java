f66ed1667c3de2749187a8d201a394c129eaaf0c
Jai Gora <Jaigora24@gmail.com>
2021-02-21 22:56:31 +0530

    Create Num_present_in_array_or_not.java

---
+++ Num_present_in_array_or_not.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.Scanner;
+public class Num_present_in_array_or_not{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the number to check: ");
+        int check = sc.nextInt();
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        boolean checker = true;
+        
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            
+            if(a[i]==check){
+                checker = true;
+                break;
+           
+            }
+            else{
+                checker = false;
+            }
+        }
+        if(checker==true){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}

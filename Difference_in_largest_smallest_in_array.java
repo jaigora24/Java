d0e835c7697b883f84ffaa1e11006442e504aaae
Jai Gora <Jaigora24@gmail.com>
2021-02-22 23:03:06 +0530

    Create Difference_in_largest_smallest_in_array.java

---
+++ Difference_in_largest_smallest_in_array.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Difference_in_largest_smallest_in_array{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int a[]=new int[n];
+        
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();
+        }
+        
+        int largest = a[0];
+        int smallest = a[0];
+        
+        for(int i=0; i<n; i++){
+            if(a[i]>largest){
+                largest = a[i];
+            }
+            if(a[i]<smallest){
+                smallest=a[i];
+            }
+        }
+        int result = largest-smallest;
+        System.out.println("Difference between largest & smallest is : "+ result);
+        
+    }
+}

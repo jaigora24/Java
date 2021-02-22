0cc8ce0fa1b38841b2ded09239ad7357224599b8
Jai Gora <Jaigora24@gmail.com>
2021-02-22 12:34:21 +0530

    Create num_of_negative_positive_in_array.java

---
+++ num_of_negative_positive_in_array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class num_of_negative_positive_in_array{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        int positive = 0;                     //declare each variable be zero
+        int negative = 0;
+        int nill = 0;
+        
+        for(int i=0; i<n; i++){
+            a[i]=sc.nextInt();                //taking array input
+        }
+        
+        for(int i=0; i<n; i++){
+            if(a[i]>0){
+                positive++;                     //if no. is greater than 0 then add 1 in counting
+            }
+            if(a[i]<0){
+                negative++;
+            }
+            if(a[i]==0){
+                nill++;
+            }
+        }
+        System.out.println(positive+" "+negative+" "+nill);         
+        
+    }
+}

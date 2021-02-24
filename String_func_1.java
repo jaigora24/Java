1332d36607f51ff33206729141ec32e95d43f68c
Jai Gora <Jaigora24@gmail.com>
2021-02-24 22:32:14 +0530

    Create String_func_1.java

---
+++ String_func_1.java
@@ -0,0 +1,12 @@
+package string;
+
+public class String_func_1 {
+
+	public static void main(String[] args) {
+		
+		String name = "Hello myself Jai Gora";
+		System.out.println(name.charAt(6));
+
+	}
+
+}

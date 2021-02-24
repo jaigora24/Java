2b639e3379935656ecc4b9efafe5f7721706c530
Jai Gora <Jaigora24@gmail.com>
2021-02-25 00:15:09 +0530

    Create String_func_6.java

---
+++ String_func_6.java
@@ -0,0 +1,13 @@
+package string;
+
+public class String_func_6 {
+
+	public static void main(String[] args) {
+		
+		String name = "Jai Gora";
+		String name2 = new String("Jai Gora");
+		System.out.println(name.equals(name2));
+
+	}
+
+}

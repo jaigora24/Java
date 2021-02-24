c9e4148dbaccf53ba9cef222950b6a1d321eb456
Jai Gora <Jaigora24@gmail.com>
2021-02-24 23:06:44 +0530

    Create String_func_5.java

---
+++ String_func_5.java
@@ -0,0 +1,11 @@
+package string;
+
+public class String_func_5 {
+
+	public static void main(String[] args) {
+		
+		String name = "Hello World";
+		System.out.println(name.contains("He"));
+	}
+
+}

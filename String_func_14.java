6c3339a032eca7fc7a37d0aa79cb7825309ba557
Jai Gora <Jaigora24@gmail.com>
2021-02-25 10:47:52 +0530

    Create String_func_14.java

---
+++ String_func_14.java
@@ -0,0 +1,12 @@
+package string;
+
+public class String_func_14 {
+
+	public static void main(String[] args) {
+		
+		String name = "    Jai Gora    ";
+		System.out.println(name.trim());
+		
+	}
+
+}

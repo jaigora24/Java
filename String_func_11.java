1c0b8736d7113d0346466badf608b3e47cc59b58
Jai Gora <Jaigora24@gmail.com>
2021-02-25 10:40:46 +0530

    Create String_func_11.java

---
+++ String_func_11.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Function_11 {
+
+	public static void main(String[] args) {
+		
+		String name = "Hello Jai Gora";
+		System.out.println(name.toLowerCase());
+	}
+
+}

51ed095813c529debab9785c3caf2ee4f9f88279
Jai Gora <Jaigora24@gmail.com>
2021-03-21 00:15:53 +0530

    Update num_of_negative_positive_in_array.java

--- num_of_negative_positive_in_array.java
+++ num_of_negative_positive_in_array.java
@@ -5,17 +5,17 @@ public class num_of_negative_positive_in_array{
         
         int n = sc.nextInt();
         int a[] = new int[n];
-        int positive = 0;                     //declare each variable be zero
+        int positive = 0;                       //declare each variable be zero
         int negative = 0;
         int nill = 0;
         
         for(int i=0; i<n; i++){
-            a[i]=sc.nextInt();                //taking array input
+            a[i]=sc.nextInt();                  //taking array input
         }
         
         for(int i=0; i<n; i++){
             if(a[i]>0){
-                positive++;                   //if no. is greater than 0 then add 1 in counting
+                positive++;                     //if no. is greater than 0 then add 1 in counting
             }
             if(a[i]<0){
                 negative++;

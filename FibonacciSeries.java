2d3e4e791c9286e5bd42eba9bb18c78485e4231c
Jai Gora <Jaigora24@gmail.com>
2021-03-23 17:17:25 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
-public class p2{
+public class main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		int fc = 0;
-		int a = 0;
-		int b = 1;
-		
-		System.out.println(a);
-		System.out.println(b);
-		
-		for (int i=1; i<=n-2; i++){
-			fc = a + b;
-			System.out.println(fc);
-			a=b;
-			b=fc;
+		if(n>1){
+			int a = 0;
+			int b = 1;
+			int c = 1;
+			System.out.println(a);
+			System.out.println(b);
+			
+			for(int i=1; i<=n-2; i++){
+				c = a+b;
+				System.out.println(c);
+				a=b;
+				b=c;
+			}
 		}
+		else if(n==1){
+			System.out.println("0");
+		}
+		else{
+			System.out.println("Wrong input");
+		}	
 	}
 }
-
-			

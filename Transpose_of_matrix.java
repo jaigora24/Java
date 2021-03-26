acda0b3e6f8f51bc5722c64306120511dec561a7
Jai Gora <Jaigora24@gmail.com>
2021-03-26 16:59:45 +0530

    Create Transpose_of_matrix.java

---
+++ Transpose_of_matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Transpose_of_matrix{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int rows = sc.nextInt();
+		int cols = sc.nextInt();
+		int a[][] = new int[rows][cols];
+		int b[][] = new int[rows][cols];
+		
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++){
+				a[i][j]=sc.nextInt();
+			}
+		}
+		
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++){
+				b[i][j]=a[j][i];
+			}
+		}
+		
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++){
+				System.out.print(b[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
